7f93ae0de2cf5c59a6af72a64e26af54d91b37a7
Isaac Fiuza <fiuzaisaac@hotmail.com>
2019-05-22 16:39:45 -0600

    adding style.css and initial content to game.js

---
+++ assets/javascript/game.js
@@ -0,0 +1,37 @@
+var wins = 0;
+var losses = 0;
+
+var maxErrors = 9;
+
+var wordDisplayLettersElement = document.getElementById("word-display-letters");
+var guessedLettersElement = document.getElementById("guessed-letters");
+var errorCountElement = document.getElementById("error-count");
+var winCountElement = document.getElementById("win-count");
+var lossCountElement = document.getElementById("loss-count");
+
+var blinkElements = document.getElementsByClassName("blinking");
+var alertLineElements = document.getElementsByClassName("alert-line");
+
+var validGuesses = [ 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' ];
+
+var pressAnyKeyToStart = ["Press Any Key To Start:"];
+var pressAnyKeyToReset = ["Press Any Key To Restart:"];
+
+var youWin = ["You Win!"];
+var youLose = ["You Lose"];
+var emptyAlert = ["                       "];
+
+var game = new Hangman();
+
+document.onkeyup = function(event) {
+	var userGuess = event.key;
+
+	if (!game.gameOver) {
+		if (validGuesses.includes(userGuess) && !game.guessedLetters.includes(userGuess)) {
+			game.checkGuess(userGuess);
+		}
+	} else {
+		game = new Hangman();
+		game.updatePageData();
+	}
+}
